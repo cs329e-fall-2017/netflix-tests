6c45894df04ea2bcb8ae6802181b5eabf3e6bac6
nmakkapati <nmakkapati@gmail.com>
2017-09-27 18:41:43 -0500

    Naren and JT Morris Acceptance Test Commit

---
+++ nm25879-jtm3574-TestNetflix.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+
+# -------
+# imports
+# -------
+from Netflix import netflix_eval
+from unittest import main, TestCase
+from math import sqrt
+from io import StringIO
+from numpy import sqrt, square, mean, subtract
+
+# -----------
+# TestNetflix
+# -----------
+
+class TestNetflix (TestCase):
+
+    # ----
+    # eval
+    # ----
+
+    def test_eval_1(self):
+        r = StringIO("10040:\n2417853\n1207062\n2487973\n")
+        w = StringIO()
+        netflix_eval(r, w)
+        self.assertEqual(
+            w.getvalue(), "10040:\n2.913\n2.913\n2.913\n1.03\n")
+
+    def test_eval_2(self):
+        r = StringIO("2043:\n1417435\n2312054\n462685\n")
+        w = StringIO()
+        netflix_eval(r, w)
+        self.assertEqual(
+            w.getvalue(), "2043:\n3.778\n3.778\n3.778\n1.67\n")
+
+    def test_eval_3(self):
+        r = StringIO("7233:\n1595251\n")
+        w = StringIO()
+        netflix_eval(r, w)
+        self.assertEqual(
+            w.getvalue(), "7233:\n4.173\n1.17\n")
+
+
+
+
+
+
+
+# ----
+# main
+# ----			
+if __name__ == '__main__':
+    main()
+
+""" #pragma: no cover
+% coverage3 run --branch TestNetflix.py >  TestNetflix.out 2>&1
+
+
+
+% coverage3 report -m                   >> TestNetflix.out
+
+
+
+% cat TestNetflix.out
+.
+----------------------------------------------------------------------
+Ran 1 test in 0.000s
+
+OK
+Name             Stmts   Miss Branch BrPart  Cover   Missing
+------------------------------------------------------------
+Netflix.py          27      0      4      0   100%
+TestNetflix.py      13      0      0      0   100%
+------------------------------------------------------------
+TOTAL               40      0      4      0   100%
+
+"""
